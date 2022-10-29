caf13eea31bbea737a91c7b04536b6f58e468425
hanjy-slowturtle <h920915@gmail.com>
2022-10-30 00:47:54 +0900

    ~ cookie 3 - 쇼핑카트 1

--- app_cookie.js
+++ app_cookie.js
@@ -10,4 +10,26 @@ app.get("/count", (req, res) => {
   res.send("count: " + count);
 });
 
+const products = {
+  1: { title: "The history of web 1" },
+  2: { title: "new web" },
+};
+app.get("/products", (req, res) => {
+  let output = "";
+  for (const key in products) {
+    output += `
+      <li>
+        <a href="/cart/${key}">${products[key].title}</a>
+      </li>
+    `;
+  }
+  res.send(`
+    <h1>Products</h1>
+    <ul>
+      ${output}
+    </ul>
+    <a href="/cart">Cart</a>
+  `);
+});
+
 app.listen(3003, () => console.log("server on 3003"));
